286c9d14bf4cd62f7b2c0383d7f52c4ed67c47fa
zahransajid <zahransajid3@gmail.com>
2022-11-17 12:08:56 +0530

    Fixed main.py to be in line with project

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from bacteria import Colony, Simulator
+from dfa import DFA, TransitionFunction
 
 
 def main():
-    sim = Simulator()
-    col = Colony(sim)
+    tf = TransitionFunction("definitions/bacteria_automata.csv")
+    Q = tf.states
+    Q0 = 0
+    F = 5
+    automaton = DFA(Q, Q0, F, tf)
+    states = {
+        0: "Initial",
+        1: "Compact Structure",
+        2: "Dense Branching",
+        3: "Meagerly Spaced Branching",
+        4: "Sparse Branching",
+        5: "Dead State",
+    }
 
-    print(col.growthVec, col.posVec)
+    for k in states.keys():
+        print(f"{k} : {states[k]}")
+
+    while True:
+        print("The current state of the DFA is", states[automaton.current])
+        print("Enter the strategy to adopt (a-e), Q to quit: ")
+        strat = input(">>> ")
+        if strat.upper() == "Q":
+            break
+        print("#" * 10)
+        automaton.run(strat)
+    print("Thank you for playing!")
 
 
 if __name__ == "__main__":
